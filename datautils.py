6f18bb0e35559a3f15a2ff990f65d2942fbad3be
navaaliqbal <119072966+navaaliqbal@users.noreply.github.com>
2025-07-28 16:14:24 +0500

    Change data processing

--- datautils.py
+++ datautils.py
@@ -1,6 +1,5 @@
 import numpy as np
-from scipy.io import arff
-
+from sklearn.model_selection import train_test_split
 
 def padding_varying_length(data):
     for i in range(data.shape[0]):
@@ -8,72 +7,27 @@ def padding_varying_length(data):
             data[i, j, :][np.isnan(data[i, j, :])] = 0
     return data
 
+def load_UCR(Path='../../archives/UCR_UEA/Multivariate_arff/', folder='Cricket', test_size=0.2, random_state=42):
+    # Load full data
+    X = np.load("/content/drive/MyDrive/FD/X.npy")  # shape (N, C, T)
+    y = np.load("/content/drive/MyDrive/FD/y.npy")  # shape (N,)
 
-def load_UCR(Path='../../archives/UCR_UEA/Multivariate_arff/', folder='Cricket'):
-    train_path = Path + folder + '/' + folder + '_TRAIN.arff'
-    test_path = Path + folder + '/' + folder + '_TEST.arff'
-    TRAIN_DATA = []
-    TRAIN_LABEL = []
-    label_dict = {}
-    label_index = 0
-    with open(train_path, encoding='UTF-8', errors='ignore') as f:
-        data, meta = arff.loadarff(f)
-        f.close()
-    if type(data[0][0]) == np.ndarray:  # multivariate
-        for index in range(data.shape[0]):
-            raw_data = data[index][0]
-            raw_label = data[index][1]
-            if label_dict.__contains__(raw_label):
-                TRAIN_LABEL.append(label_dict[raw_label])
-            else:
-                label_dict[raw_label] = label_index
-                TRAIN_LABEL.append(label_index)
-                label_index += 1
-            raw_data_list = raw_data.tolist()
-            # print(raw_data_list)
-            TRAIN_DATA.append(np.array(raw_data_list).astype(np.float32).transpose(-1, 0))
+    # Transpose to (N, T, C)
+    X = X.transpose(0, 2, 1)
 
-        TEST_DATA = []
-        TEST_LABEL = []
-        with open(test_path, encoding='UTF-8', errors='ignore') as f:
-            data, meta = arff.loadarff(f)
-            f.close()
-        for index in range(data.shape[0]):
-            raw_data = data[index][0]
-            raw_label = data[index][1]
-            TEST_LABEL.append(label_dict[raw_label])
-            raw_data_list = raw_data.tolist()
-            TEST_DATA.append(np.array(raw_data_list).astype(np.float32).transpose(-1, 0))
+    # Optional: truncate sequence length
+    MAX_TIME_STEPS = 30000
+    X = X[:, :MAX_TIME_STEPS, :]
 
-        TRAIN_DATA = padding_varying_length(np.array(TRAIN_DATA))
-        TEST_DATA = padding_varying_length(np.array(TEST_DATA))
+    # Train-test split
+    X_train, X_test, y_train, y_test = train_test_split(
+        X, y, test_size=test_size, random_state=random_state, stratify=y
+    )
 
-        return [np.array(TRAIN_DATA), np.array(TRAIN_LABEL)], [np.array(TEST_DATA), np.array(TEST_LABEL)]
+    # Ensure correct types
+    X_train = X_train.astype(np.float32)
+    X_test = X_test.astype(np.float32)
+    y_train = y_train.astype(np.int64)
+    y_test = y_test.astype(np.int64)
 
-    else:  # univariate
-        for index in range(data.shape[0]):
-            raw_data = np.array(list(data[index]))[:-1]
-            raw_label = data[index][-1]
-            if label_dict.__contains__(raw_label):
-                TRAIN_LABEL.append(label_dict[raw_label])
-            else:
-                label_dict[raw_label] = label_index
-                TRAIN_LABEL.append(label_index)
-                label_index += 1
-            TRAIN_DATA.append(np.array(raw_data).astype(np.float32).reshape(-1, 1))
-
-        TEST_DATA = []
-        TEST_LABEL = []
-        with open(test_path, encoding='UTF-8', errors='ignore') as f:
-            data, meta = arff.loadarff(f)
-            f.close()
-        for index in range(data.shape[0]):
-            raw_data = np.array(list(data[index]))[:-1]
-            raw_label = data[index][-1]
-            TEST_LABEL.append(label_dict[raw_label])
-            TEST_DATA.append(np.array(raw_data).astype(np.float32).reshape(-1, 1))
-
-        TRAIN_DATA = padding_varying_length(np.array(TRAIN_DATA))
-        TEST_DATA = padding_varying_length(np.array(TEST_DATA))
-
-        return [np.array(TRAIN_DATA), np.array(TRAIN_LABEL)], [np.array(TEST_DATA), np.array(TEST_LABEL)]
+    return (X_train, y_train), (X_test, y_test)
